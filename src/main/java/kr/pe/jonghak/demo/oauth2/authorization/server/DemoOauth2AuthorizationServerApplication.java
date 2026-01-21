0de6015c4872de31daa69015e6cab9760b493141
jhlee7854 <jhlee7854@me.com>
2026-01-21 19:17:14 +0900

    Initial commit

---
+++ src/main/java/kr/pe/jonghak/demo/oauth2/authorization/server/DemoOauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.demo.oauth2.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauth2AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOauth2AuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pe/jonghak/demo/oauth2/authorization/server/DemoOauth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.demo.oauth2.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOauth2AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
